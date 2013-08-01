d7eefcdc397583a28b05faf4048d1ee6a4f78af5
Alexandre Courtemanche <alex.courtemanche1@gmail.com>
2013-08-01 13:49:25 -0400

    Switched svd parser to the one used in https://github.com/bnahill/PyCortexMDebug

--- src/regmapview.py
+++ src/regmapview.py
@@ -11,8 +11,9 @@ import mmap
 import struct
 import optparse
 import cmd
+
 import cPickle as pickle
-from regmap import *
+from svd import *
 
 
 class RegMapViewer(cmd.Cmd, object):	
@@ -22,60 +23,45 @@ class RegMapViewer(cmd.Cmd, object):
 		cmd.Cmd.__init__(self)
 		#self.regmap_o = RegMap('../svd/iMX6DQ.svd.xml')
 		self.reset_autocomplete()
-		'''
-		if self.regmap_o == None:
-			print('Loading '+ filename)
-			self.regmap_o = pickle.load(open('iMX6DQ.p', 'rb'))
-		'''
+
 	def reset_autocomplete(self):
 		self.rr_num_words_in_line = 0
 		self.rr_completed_prefix = False	
 		self.rr_complete_reglist = []
 
-	def do_createregmap(self, dumpfile= './iMX6DQ.p', svd = '../svd/iMX6DQ.svd.xml'):
-		print('Parsing file')
-		rm = RegMap(svd)
-		print('Serializing...')
-		pickle.dump( rm, open('./iMX6DQ.p', 'wb'))
-		print('Saved to file '+ dumpfile)
-
 	def do_loadregmap(self, filename):
-		self.reset_autocomplete()
+		if filename == '':
+			filename = "../svd/iMX6DQ.svd.xml"
 		print('Loading '+ filename)
-		self.regmap_o = pickle.load(open('iMX6DQ.p', 'rb'))
+		self.svd = SVDFile(filename)
+
 
 	def do_readreg(self, addr = 0):
 		"""Retrieves the contents of a particular register"""
 		content = 0
 		# Reset the autocomplete parameters
-		self.rr_num_words_in_line = 0
-		self.rr_completed_prefix = False	
-		self.rr_complete_reglist = []
+		self.reset_autocomplete()
 		output_str = 'Content of register {0} is {1}'.format(addr, content)
 		print(output_str)
-		#print(self.regmap_o.reglists()[84])
 
 	def complete_readreg(self, text, line, beginidx, endidx):
 		mline = line.partition(' ')[2] # Get the string after the first space
-		prefixes = self.regmap_o.get_periph_prefixes()
-
+		prefixes = []
+		for p in self.svd.peripherals.iteritems():
+			prefixes.append(p[0] + '_')
+		print(mline in prefixes)
+		print('mline = ' + mline)
 		if (mline in prefixes):
 			self.rr_completed_prefix = True
+			self.rr_chosen_periph_str = mline.split('_')[0]
 			# Build reg list
-			print('got in')
-			self.rr_complete_reglist = self.regmap_o.get_reg_names(mline)
-			print('got out')
-			'''
-			periph_index = prefixes.index(mline)
-			for reg in self.regmap_o.get_reg_names()[periph_index]:
-				self.rr_complete_reglist.append(prefixes[periph_index]+reg)
-			'''
+			for reg in self.svd.peripherals[self.rr_chosen_periph_str].registers:
+				self.rr_complete_reglist.append(self.rr_chosen_periph_str + '_' +reg)
 			return [s for s in self.rr_complete_reglist if s.startswith(mline)]
 		else:
 			if self.rr_completed_prefix: # The line contains the prefix and something more
 				return [s for s in self.rr_complete_reglist if s.startswith(mline)]
 			else: # User has not yet completed the prefix
-				print('hi')
 				return [s for s in prefixes if s.startswith(mline)]
 
 	def do_writereg(self, addr, content):
@@ -98,6 +84,7 @@ class RegMapViewer(cmd.Cmd, object):
 
 	def do_getfield(self, register, field):
 		"""Retrieves the contents of a particular field"""
+		
 		content = 0
 		output_str = 'Content of field {0} of register {1} is {2}'.format(register, field, content)
 		print(output_str)
